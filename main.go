634fce565cf7a7cf3b91920190d69ffada1f0bc8
Chabu Chabu <chabu.chabu@episerver.com>
2023-01-17 23:43:10 +0000

    adding content to faq page

--- main.go
+++ main.go
@@ -17,10 +17,26 @@ func contactHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:chabuchabu32@gmail.com\">chabuchabu32@gmail.com</a>.</p>")
 }
 
-func faqHandler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
-	fmt.Fprint(w,"<h1>Frequently asked questions</h1>")
-}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+  <ul>
+	<li>
+	  <b>Is there a free version?</b>
+	  Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+	  <b>What are your support hours?</b>
+	  We have support staff answering emails 24/7, though response
+	  times may be a bit slower on weekends.
+	</li>
+	<li>
+	  <b>How do I contact support?</b>
+	  Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+	</li>
+  </ul>
+  `)
+  }
 
 func main() {
 	r := chi.NewRouter()
