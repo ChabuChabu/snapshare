6def933cb3957681864bdb51136b84ada8cf37c7
Chabu Chabu <chabu.chabu@episerver.com>
2023-01-22 22:28:48 +0000

    Refactoring the homeHandler to use an html template

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
-import(
+import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
-	fmt.Fprint(w,"<h1>Welcome to my awoseome site!")
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("Parsing templates: %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing a templates: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
-	fmt.Fprint(w,"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:chabuchabu32@gmail.com\">chabuchabu32@gmail.com</a>.</p>")
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:chabuchabu32@gmail.com\">chabuchabu32@gmail.com</a>.</p>")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,16 +51,16 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	</li>
   </ul>
   `)
-  }
+}
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/",homeHandler)
-	r.Get("/contact",contactHandler)
-	r.Get("/faq",faqHandler)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request){
-		http.Error(w,"Page not found",http.StatusNotFound)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on :3000....")
 	http.ListenAndServe(":3000", r)
- }
+}
